e0bb515752062bd193ff84d9a6f11bb0f0abf36c
yangguang <191907466@qq.com>
2018-04-05 09:16:38 +0800

    practice008

---
+++ 20180405/practice008.c
@@ -0,0 +1,16 @@
+/*要求输出国际象棋棋盘。*/
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	for(i=0;i<8;i++)
+	{	
+		for(j=0;j<8;j++)
+		if((i+j)%2==0)
+			printf("%c%c",'\xA1','\xF6');
+		else
+			printf("  ");
+		printf("\n");
+	}
+	return 0;
+}
